3d5571f53c00828016f2f50c02901aea52420fe7
Nugar <cladeadenugar@gmail.com>
2024-01-24 12:15:14 +0100

    added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import { useMediaQuery } from "@mui/material";
 import IphoneMain from "../assets/iphone-main.png";
 import IphoneSlant from "../assets/iphone-slant.png";
 import Notification from "../assets/Notification.png";
+import GreenEllipse from "../assets/green-ellipse.png";
 
 const Hero = ({ dmSans, mulish }) => {
   const isNonMobileScreens = useMediaQuery("(min-width:1024px)");
@@ -95,25 +96,37 @@ const Hero = ({ dmSans, mulish }) => {
                   alt="IphoneMainBody"
                   className=" h-[541.699px] flex-shrink-0"
                 />
-                <svg
-                className=""
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="329"
-                  height="57"
-                  viewBox="0 0 329 57"
-                  fill="none"
-                >
-                  <path
-                    d="M327 1C308.46 35.5 254.246 55 164.5 55C74.7537 55 2 30.8234 2 1"
-                    stroke="#92E3A9"
-                    stroke-width="4"
+
+                <div className=" absolute  flex ">
+                  <img
+                    src={GreenEllipse}
+                    alt="GreenEllipse"
+                    className="ml-[-28px] mt-[-35px]"
                   />
-                </svg>
+
+                  <div className="bg-[#13E24E] rounded-full w-[60px] h-[60px] ml-[-35px] flex justify-center items-center">
+                    <svg
+                      width="28"
+                      height="28"
+                      viewBox="0 0 28 28"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g id="ph:paper-plane-tilt-fill">
+                        <path
+                          id="Vector"
+                          d="M25.3087 4.8498V4.86621L18.9431 25.8596C18.8467 26.2006 18.6484 26.504 18.3747 26.729C18.101 26.954 17.765 27.09 17.4118 27.1185C17.3615 27.1229 17.3112 27.1251 17.2609 27.1251C16.9299 27.1261 16.6055 27.0325 16.3261 26.8551C16.0466 26.6778 15.8237 26.4242 15.6837 26.1243L11.7779 17.8785C11.7394 17.797 11.727 17.7055 11.7424 17.6166C11.7579 17.5277 11.8005 17.4457 11.8643 17.382L18.1271 11.1181C18.2843 10.9527 18.3706 10.7324 18.3677 10.5042C18.3648 10.276 18.2728 10.058 18.1115 9.89669C17.9501 9.73533 17.7321 9.6434 17.504 9.64048C17.2758 9.63755 17.0555 9.72388 16.8901 9.88105L10.6174 16.1439C10.5537 16.2077 10.4718 16.2503 10.3829 16.2658C10.294 16.2812 10.2025 16.2688 10.1209 16.2303L1.91776 12.3442C1.59559 12.1966 1.32446 11.9567 1.13872 11.6549C0.952968 11.3531 0.860953 11.003 0.874322 10.6489C0.892137 10.2849 1.0239 9.93574 1.25099 9.65076C1.47808 9.36577 1.78899 9.15937 2.13979 9.06074L23.1332 2.69511H23.1496C23.4485 2.61113 23.7644 2.60819 24.0648 2.68658C24.3652 2.76497 24.6394 2.92187 24.8591 3.14119C25.0789 3.36051 25.2364 3.63434 25.3154 3.9346C25.3944 4.23486 25.3921 4.55073 25.3087 4.8498Z"
+                          fill="white"
+                        />
+                      </g>
+                    </svg>
+                  </div>
+                </div>
               </div>
               <img
                 src={IphoneSlant}
                 alt="IphoneSlant"
-                className=" h-[553.434px] z-20  flex-shrink-0"
+                className=" h-[553.434px]   flex-shrink-0"
               />
             </div>
           </div>
