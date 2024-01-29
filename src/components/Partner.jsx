b0eb8080362e311d5f993e33b758f1235c9c77b4
Nugar <cladeadenugar@gmail.com>
2024-01-29 03:55:49 +0100

    added

--- src/components/Partner.jsx
+++ src/components/Partner.jsx
@@ -1,8 +1,26 @@
 import { useMediaQuery } from "@mui/material";
 
-const Partner = () => {
+const Partner = ({ mulish }) => {
   const isNonMobileScreens = useMediaQuery("(min-width:768px)");
-  return <div>{isNonMobileScreens ? <div></div> : <div></div>}</div>;
+  return (
+    <div>
+      {isNonMobileScreens ? (
+        <div style={mulish} className="py-20">
+          <h1 className="text-4xl font-bold text-center">
+            Partner with <span className="text-[#5D32E3]">Over 20 </span>
+            service providers in Nigeria
+          </h1>
+        </div>
+      ) : (
+        <div style={mulish} className="py-16 z-20">
+          <h1 className="text-2xl font-bold text-center mx-auto max-w-[320px]">
+            Partner with <span className="text-[#5D32E3]">Over 20 </span>
+            service providers in Nigeria
+          </h1>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Partner;
