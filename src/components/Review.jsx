83b14bccd075c664cc48a2def9f039ca4f67c3a7
Nugar <cladeadenugar@gmail.com>
2024-02-07 06:05:29 +0100

    added

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -2,11 +2,28 @@ import { useMediaQuery } from "@mui/material";
 
 const Review = ({ mulish, dmSans }) => {
   const isNonMobileScreens = useMediaQuery("(min-width:900px)");
+  const middleDivStyle = {
+    position: "absolute",
+    width: "480px",
+    height: "480px",
+    background:
+      "linear-gradient(128.14deg, rgba(146, 227, 169, 0.65) 22.59%, rgba(245, 174, 35, 0.36) 47.12%, #5D87E1 72.17%)",
+    filter: "blur(290px)",
+    left: "calc(50% - 480px/2)",
+    top: "calc(50% - 480px/2 + 19px)",
+  };
+
+  const leftDivStyle = {
+
+  }
   return (
     <div>
       {isNonMobileScreens ? (
-        <div className="mt-24 bg-[#121621] h-screen px-10 xl:px-16 py-24">
-          <div></div>
+        <div className="mt-24 bg-[#121621] relative h-screen px-10 xl:px-16 py-24">
+          <div
+            style={middleDivStyle}
+            className="flex justify-center items-center"
+          ></div>
           <div></div>
           <div className="flex items-center space-x-3">
             <h1 style={dmSans} className="text-white text-opacity-60">
@@ -23,13 +40,9 @@ const Review = ({ mulish, dmSans }) => {
         </div>
       ) : (
         <div>
-          /* Ellipse 436 */ position: absolute; width: 480px; height: 480px;
-          left: calc(50% - 480px/2); top: calc(50% - 480px/2 + 19px);
-          background: linear-gradient(128.14deg, rgba(146, 227, 169, 0.65)
-          22.59%, rgba(245, 174, 35, 0.36) 47.12%, #5D87E1 72.17%);
-          mix-blend-mode: normal; filter: blur(290px); /* Ellipse 438 */
-          position: absolute; width: 72px; height: 72px; left: 94px; top: 420px;
-          background: #5EC4F0; filter: blur(70px);
+          /* Ellipse 436 */ /* Ellipse 438 */ position: "absolute; width: 72px;
+          height: 72px; left: 94px; top: 420px; background: #5EC4F0; filter:
+          blur(70px);
         </div>
       )}
     </div>
